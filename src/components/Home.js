c700e38ea6024449ca26979e0049316f1bd08684
Priya <priya_93_x@hotmail.co.uk>
2025-02-17 12:34:15 +0000

    added logout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import logoutIcon from '../images/logout.png'
 
 function Home() {
+  const navigate = useNavigate();
+
+  function logout() {
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("refresh_token");
+    navigate('/login');
+  }
+
   return (
-    <div className="text-center font-mono border-b md:flex items-center py-6 bg-teal-100">
+    <div className="text-center font-mono border-b md:flex items-center py-6 bg-teal-100 relative">
       <h1 className="text-4xl font-bold px-5">Go-Lingo</h1>
       <p className="pt-2 text-xl px-5">Connecting places to phrases</p>
+      <img src={logoutIcon} className="absolute right-0 m-4 font-xl hover:scale-105" alt="logout" onClick={logout} width="60"/>
     </div>
   )
 }
